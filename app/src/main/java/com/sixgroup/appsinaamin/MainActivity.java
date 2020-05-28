5b3a53b9202d92ee22b48008e3cdfcb6bb4cf5dd
Sina Fuchs <sinafuchs@Sinas-MacBook-Pro.local>
2020-05-28 16:26:40 +0200

    login und registrieren

--- app/src/main/java/com/sixgroup/appsinaamin/MainActivity.java
+++ app/src/main/java/com/sixgroup/appsinaamin/MainActivity.java
@@ -8,11 +8,17 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textViewZuRegistrieren = findViewById(R.id.textViewzuRegistrieren);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrieren);
-
+        setContentView(R.layout.activity_main);
+        
     }
 
+    public void goToRegistrieren (View view){
+        Intent intent = new Intent (this, RegistrierenActivity.class);
+        startActivity(intent);
+    }
 }
