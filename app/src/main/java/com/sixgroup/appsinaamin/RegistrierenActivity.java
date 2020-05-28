f0a4d18e81ff4c98f3be120fac428109495d3e15
Sina Fuchs <sinafuchs@Sinas-MacBook-Pro.local>
2020-05-28 15:30:13 +0200

    login und registrieren

--- app/src/main/java/com/sixgroup/appsinaamin/RegistrierenActivity.java
+++ app/src/main/java/com/sixgroup/appsinaamin/RegistrierenActivity.java
@@ -1,18 +1,14 @@
 package com.sixgroup.appsinaamin;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrierenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrieren);
-
     }
-
 }
