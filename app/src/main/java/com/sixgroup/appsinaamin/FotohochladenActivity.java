3657ffbe621e9fbc6ebb6fe46a1126ae4b058b64
sinafuchs <sina.fuchs@gmx.ch>
2020-05-29 10:09:53 +0200

    switching between all activitys

--- app/src/main/java/com/sixgroup/appsinaamin/FotohochladenActivity.java
+++ app/src/main/java/com/sixgroup/appsinaamin/FotohochladenActivity.java
@@ -1,22 +1,17 @@
 package com.sixgroup.appsinaamin;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FotohochladenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void goToRegistrieren (View view){
-        Intent intent = new Intent (this, RegistrierenActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_fotohochladen);
     }
 
     public void goToLogedin (View view){
